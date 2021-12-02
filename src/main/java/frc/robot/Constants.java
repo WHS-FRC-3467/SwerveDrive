645d6667600ec13616156ea3f79b9ef5ab2c7dd8
Programming 2 <apanteli23@team3467.org>
2021-12-01 20:42:50 -0500

    Changed offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,17 +31,17 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9; 
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(75.14);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
     
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 5; 
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6; 
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10; 
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(119.00); 
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); 
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3; 
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 4;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11; 
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(323.34); 
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); 
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
